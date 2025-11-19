5dfe94e19faf403b06d74357abbe8c6699eafd2c
swifterpro123 <swifter0pro@gmail.com>
2025-11-18 19:12:27 -0500

    added slashing with sword

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,33 +1,44 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+var is_slashing: bool = false
+
+func _ready() -> void:
+	animated_sprite.animation_finished.connect(Callable(self, "_on_animation_finished"))
+
+func _on_animation_finished() -> void:
+	if animated_sprite.animation == "slash":
+		is_slashing = false
+		animated_sprite.play('idle')
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	if Input.is_action_just_pressed("slash") and not is_slashing:
+		is_slashing = true
+		animated_sprite.play("slash")
+
+	# movement input
 	var direction := Input.get_axis("move_left", "move_right")
-	
-	if direction > 0:
-		animated_sprite.flip_h = true
-		animated_sprite.play("run")
-	elif direction < 0:
-		animated_sprite.flip_h = false
-		animated_sprite.play("run")
-	else:
-		animated_sprite.play("idle")
-	if direction:
+
+	if not is_slashing:
+		if direction > 0:
+			animated_sprite.flip_h = true
+			animated_sprite.play("run")
+		elif direction < 0:
+			animated_sprite.flip_h = false
+			animated_sprite.play("run")
+		else:
+			animated_sprite.play("idle")
+
+	if direction != 0:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
