cc538e4a64e74febd4ca3fb490435537455717c9
adi01776 <adriantk2000@gmail.com>
2026-01-18 21:37:43 -0500

    polished tutorial

--- scripts/menu_screen.gd
+++ scripts/menu_screen.gd
@@ -6,4 +6,4 @@ func _ready():
 func _unhandled_input(event):
 	if event is InputEventKey and event.pressed:
 		visible = false
-		get_tree().root.get_node("Bgmusic").play_tutorial_music()
+		#get_tree().root.get_node("Bgmusic").play_tutorial_music()
